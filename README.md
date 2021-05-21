b1659d39774fa27bce12d6621bd2ca80643fc912
0x37U <sagdahane@gmail.com>
2021-05-21 17:42:05 +0200

    subs

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# SubDomain Finder Tool
+
+```bash
+$ python sub.py
+```
+
+# How to use
+ - Run Script
+ - Put Target Link
+ - Go to Sub.txt
+
+![](./img.png)
---
+++ sub.py
@@ -0,0 +1,40 @@
+from requests import get
+
+import os
+
+os.system("cls")
+
+print("""
+
+ ██████╗ ██╗  ██╗██████╗ ███████╗██╗   ██╗
+██╔═████╗╚██╗██╔╝╚════██╗╚════██║██║   ██║
+██║██╔██║ ╚███╔╝  █████╔╝    ██╔╝██║   ██║
+████╔╝██║ ██╔██╗  ╚═══██╗   ██╔╝ ██║   ██║
+╚██████╔╝██╔╝ ██╗██████╔╝   ██║  ╚██████╔╝
+ ╚═════╝ ╚═╝  ╚═╝╚═════╝    ╚═╝   ╚═════╝ 
+        Github : github.com/0x37U
+
+""")
+
+os.system("title [-] 0x37U SubDomain Finder v1 [-]")
+
+Link = input("0x37U@Domain:~# ").strip()
+
+def subs(domain):
+
+    url = f"http://jamet1337.ml/api/same-domain.php?url={domain}"
+    Data = get(url,timeout=5)
+    Json = Data.json()
+    domains = Json['hasil'].split("\r")
+    if "Gagal Memuat Data" in domains:
+
+        print("Domain is Down !!")
+    else:
+
+        for i in domains:
+            print(i)
+            f = open("Subs.txt","a")
+            f.write(i)
+    
+        print("<========= Check \"Subs.txt\" File. =========>")
+subs(Link)
